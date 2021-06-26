28d5bb47965f2dc00db6909b7ffa6e781ab4d813
vgsferreira <vinicius_920@hotmail.com>
2021-06-26 01:12:01 +0000

    envio array

---
+++ array.js
@@ -0,0 +1,15 @@
+var array = []
+var convidados = ['vinicius', 'diego', 'camargo']
+
+convidados.push('Leonardo')
+
+function enviarConfirmacao (convidados){
+    
+    for(i=0; i < convidados.length; i++){
+
+        console.log(convidados[i]+ ', você foi convidado para a festa!')        
+
+    }
+}
+
+enviarConfirmacao(convidados)
